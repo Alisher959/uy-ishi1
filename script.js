d55b5b82b35d797b446113aa7fdb60d7caa4f4e8
Alisher959 <85029201+Alisher959@users.noreply.github.com>
2022-02-25 20:07:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let age = +prompt('yoshingizni kiriting ');
 if (age < 10) {
     alert('salom kichkintoy')
-} else if (age < 18 & age > 10) {
+} else if (age < 60 & age >= 10) {
     alert('salom mehmon')
-} else if (age < 110 & age > 18) {
+} else if (age < 110 & age >= 60) {
     alert('salom bobo')
 } else {
     alert('yoshingizga kirib turaylik')
